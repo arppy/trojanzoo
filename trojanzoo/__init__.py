345fc826955ba985525fa703e264d706e5ac3e37
arppy <berta@inf.u-szeged.hu>
2022-01-16 19:51:47 +0100

    torch version

--- trojanzoo/__init__.py
+++ trojanzoo/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from .version import __version__ as internal_version
-from torch.torch_version import TorchVersion
+import torch
 
 from trojanzoo import environ as environ
 from trojanzoo import datasets as datasets
@@ -12,4 +12,4 @@ from trojanzoo.utils.module import summary
 from trojanzoo.utils.tensor import to_tensor, to_numpy, to_list
 
 __all__ = ['summary', 'to_tensor', 'to_numpy', 'to_list']
-__version__ = TorchVersion(internal_version)
+__version__ = torch.__version__
